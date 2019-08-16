63f83c0ebb4e688868416f7b09629e2b5253f288
LaiYizhou <lai_yizhou@163.com>
2019-08-16 15:57:23 +0800

    add Singleton
    add Singleton

---
+++ Singleton/MonoSingleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    private static readonly object syncObject = new object();
+    public static T Instance
+    {
+        get
+        {
+            lock (syncObject)
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<T>();
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,19 @@
+﻿public class Singleton<T> where T : class, new()
+{
+    private static T instance;
+    private static readonly object syncObject = new object();
+    public static T Instance
+    {
+        get
+        {
+            lock (syncObject)
+            {
+                if (null == instance)
+                {
+                    instance = new T();
+                }
+                return instance;
+            }
+        }
+    }
+}
